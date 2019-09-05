1fd0a8d265e3e16cc07966fa02e857fa54e455f6
Drawethree <janci.kluka@gmail.com>
2019-09-05 16:01:01 +0200

    Updated

--- src/main/java/sk/drawethree/deathchestpro/utils/LocationUtil.java
+++ src/main/java/sk/drawethree/deathchestpro/utils/LocationUtil.java
@@ -5,7 +5,7 @@ import org.bukkit.Location;
 public class LocationUtil {
 
     public static Location getCenter(Location loc) {
-        return loc.add(0.5,0,0.5);
+        return loc.clone().add(0.5, 0, 0.5);
     }
 
 
