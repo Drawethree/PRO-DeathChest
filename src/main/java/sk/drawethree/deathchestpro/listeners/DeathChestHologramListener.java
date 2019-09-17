fb80bfb2b08b26cb57c6610c9536f1864d7cc85e
Drawethree <janci.kluka@gmail.com>
2019-09-17 23:34:06 +0200

    Updated algo

--- src/main/java/sk/drawethree/deathchestpro/listeners/DeathChestHologramListener.java
+++ src/main/java/sk/drawethree/deathchestpro/listeners/DeathChestHologramListener.java
@@ -2,13 +2,16 @@ package sk.drawethree.deathchestpro.listeners;
 
 import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 import sk.drawethree.deathchestpro.DeathChestPro;
 import sk.drawethree.deathchestpro.chest.DeathChest;
+import sk.drawethree.deathchestpro.chest.DeathChestHologram;
 
 
 public class DeathChestHologramListener implements Listener {
@@ -27,28 +30,8 @@ public class DeathChestHologramListener implements Listener {
             return;
         }
 
-        Chunk chunk = event.getChunk();
+        new ChunkLoadTask(event.getChunk()).runTaskLater(this.plugin, 0L);
 
-        if (chunk == null || !chunk.isLoaded()) {
-            return;
-        }
-
-        for (DeathChest dc : this.plugin.getDeathChestManager().getDeathChestsByUUID().values()) {
-
-            if(dc.getHologram().isSpawned()) {
-                continue;
-            }
-
-            if (!chunk.getWorld().equals(dc.getLocation().getWorld())) {
-                continue;
-            }
-
-            Location loc = dc.getLocation();
-
-            if (loc.getWorld().getChunkAt(loc).equals(event.getChunk())) {
-                dc.getHologram().spawn();
-            }
-        }
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
@@ -58,21 +41,66 @@ public class DeathChestHologramListener implements Listener {
             return;
         }
 
-        for (DeathChest dc : this.plugin.getDeathChestManager().getDeathChestsByUUID().values()) {
+        new ChunkUnloadTask(event.getChunk()).runTaskLater(this.plugin, 0L);
+    }
 
-            if(!dc.getHologram().isSpawned()) {
-                continue;
+    private class ChunkLoadTask extends BukkitRunnable {
+
+        private Chunk chunk;
+
+        public ChunkLoadTask(Chunk chunk) {
+            this.chunk = chunk;
+        }
+
+        @Override
+        public void run() {
+
+            if (chunk == null || !chunk.isLoaded()) {
+                return;
             }
 
-            if (!event.getChunk().getWorld().equals(dc.getLocation().getWorld())) {
-                continue;
+            //Check for remaining holograms
+            for (Entity e : chunk.getEntities()) {
+                if (e.hasMetadata(DeathChestHologram.ENTITY_METADATA)) {
+                    e.remove();
+                }
             }
 
+            /*for (DeathChest dc : plugin.getDeathChestManager().getDeathChestsByUUID().values()) {
 
-            Location loc = dc.getLocation();
+                if (dc.getHologram().isSpawned() || !chunk.getWorld().equals(dc.getLocation().getWorld())) {
+                    continue;
+                }
 
-            if (loc.getWorld().getChunkAt(loc).equals(event.getChunk())) {
-                dc.getHologram().despawn();
+                Location loc = dc.getLocation();
+
+                if (loc.getWorld().getChunkAt(loc).equals(chunk)) {
+                    dc.getHologram().spawn();
+                }
+            }*/
+        }
+    }
+
+    private class ChunkUnloadTask extends BukkitRunnable {
+        private Chunk chunk;
+
+        public ChunkUnloadTask(Chunk chunk) {
+            this.chunk = chunk;
+        }
+
+        @Override
+        public void run() {
+            for (DeathChest dc : plugin.getDeathChestManager().getDeathChestsByUUID().values()) {
+
+                if (!dc.getHologram().isSpawned() || !chunk.getWorld().equals(dc.getLocation().getWorld())) {
+                    continue;
+                }
+
+                Location loc = dc.getLocation();
+
+                if (loc.getWorld().getChunkAt(loc).equals(chunk)) {
+                    dc.getHologram().despawn();
+                }
             }
         }
     }
